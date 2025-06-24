526281154763af6bcceb10bcaa7d7e1389df3653
sw <sjugas1234@naver.com>
2025-06-24 22:03:36 +0900

    refac : 관리자 -> 사용자 관리

---
+++ MES_SW/Admin/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_SW.Admin.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Department { get; set; }
+        public bool Status { get; set; }
+
+        public Employee()
+        {
+            Name = string.Empty;
+            Role = string.Empty;
+            Department = string.Empty;
+        }
+    }
+}
---
+++ MES_SW/Data/Admin/UserManageRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_SW.Data.Admin
+{
+    public class UserManageRepository
+    {
+        public DataTable UserInfo()
+        {
+            string query = "SELECT * FROM Users";
+            return DBHelper.ExecuteDataTable(query);
+        }
+
+        public int InsertNewUserToDB(int EmployeeID, string Name, bool Status, string Role, string Department)
+        {
+            string query = @"INSERT INTO 
+                            Users (EmployeeID, UserName, UserRole, UserStatus, Department) 
+                            VALUES(@EmployeeID, @UserName, @UserRole, @UserStatus, @Department)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@EmployeeID", EmployeeID),
+                new SqlParameter("UserName", Name),
+                new SqlParameter("UserStatus", Status),
+                new SqlParameter("UserRole", Role),
+                new SqlParameter("@Department", Department)
+            };
+            return DBHelper.ExecuteNonQuery(query, parameters);
+            
+        }
+
+        public int UpdateUserToDB(int EmployeeID, string Name, bool Status, string Role, string Department)
+        {
+            string query = @"UPDATE Users 
+                            SET UserName = @UserName,
+                                UserRole = @UserRole, 
+                                UserStatus = @UserStatus, 
+                                Department = @Department 
+                                WHERE EmployeeID = @EmployeeID";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@EmployeeID", EmployeeID),
+                new SqlParameter("UserName", Name),
+                new SqlParameter("UserStatus", Status),
+                new SqlParameter("UserRole", Role),
+                new SqlParameter("Department", Department)
+            };
+            return DBHelper.ExecuteNonQuery(query, parameters);
+        }
+
+        public int DeleteUserToDB(int EmployeeID)
+        {
+            string query = "DELETE FROM Users WHERE EmployeeID = @EmployeeID";
+
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@EmployeeID", EmployeeID)
+            };
+            return DBHelper.ExecuteNonQuery(query, parameters);
+        }
+
+    }
+}
